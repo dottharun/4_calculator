0b7b09ce4c57166927359e84f49c0b6da1879130
dottharun <tharun242424@gmail.com>
2022-12-30 06:15:54 +0530

    adding  number input to result

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const result = document.querySelector(".answer");
+const anyNumberButton = document.querySelectorAll(".number");
+
+for (let i = 0; i < anyNumberButton.length; i++) {
+  anyNumberButton[i].addEventListener("click", function () {
+    result.textContent += anyNumberButton[i].textContent;
+  });
+}

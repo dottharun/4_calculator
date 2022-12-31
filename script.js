e259b13ec8498efb4f92af85acc8353b4b509c12
dottharun <tharun242424@gmail.com>
2022-12-31 21:13:43 +0530

    handle number button click

--- script.js
+++ script.js
@@ -3,12 +3,21 @@
 const result = document.querySelector(".answer");
 const anyNumberButton = document.querySelectorAll(".number");
 
+function handleNum(num) {
+  console.log(num);
+}
+
 function init() {
+  //Number button input
   for (let i = 0; i < anyNumberButton.length; i++) {
     anyNumberButton[i].addEventListener("click", function () {
-      result.textContent += anyNumberButton[i].textContent;
+      // result.textContent += anyNumberButton[i].textContent;
+      handleNum(anyNumberButton[i].textContent);
     });
   }
+
+  //Result screen
+  reRenderResult();
 }
 
 init();

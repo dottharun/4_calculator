106c85a41f63a0ec0691e250143bb6db2d152903
dottharun <tharun242424@gmail.com>
2022-12-31 22:02:44 +0530

    special and operator input functions completed

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 const result = document.querySelector(".answer");
 const anyNumberButton = document.querySelectorAll(".number");
+const anyOperatorButton = document.querySelectorAll(".operator");
+const anySpecialButton = document.querySelectorAll(".special");
 
 let buffer = "0";
 
@@ -9,6 +11,7 @@ function reRenderResult() {
   result.textContent = buffer;
 }
 
+//To handle number input
 function handleNumber(number) {
   if (buffer === "0") {
     buffer = number;
@@ -17,11 +20,41 @@ function handleNumber(number) {
   }
 }
 
+//To handle operator input
+function handleOperator(operator) {
+  console.log(operator);
+}
+
+//To handle specials
+function handleSpecial(special) {
+  console.log(special);
+}
+
 function init() {
-  //Number button input
+  //Number input
   for (let i = 0; i < anyNumberButton.length; i++) {
     anyNumberButton[i].addEventListener("click", function () {
       handleNumber(anyNumberButton[i].textContent);
+
+      //Result screen rerender
+      reRenderResult();
+    });
+  }
+
+  //Operator input
+  for (let i = 0; i < anyOperatorButton.length; i++) {
+    anyOperatorButton[i].addEventListener("click", function () {
+      handleOperator(anyOperatorButton[i].textContent);
+
+      //Result screen rerender
+      reRenderResult();
+    });
+  }
+
+  //Special input
+  for (let i = 0; i < anySpecialButton.length; i++) {
+    anySpecialButton[i].addEventListener("click", function () {
+      handleOperator(anySpecialButton[i].textContent);
 
       //Result screen rerender
       reRenderResult();

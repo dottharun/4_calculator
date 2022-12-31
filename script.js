f3b96fb2aafd42670d08f8e1f596eec524f98bad
dottharun <tharun242424@gmail.com>
2022-12-31 21:06:36 +0530

    init function

--- script.js
+++ script.js
@@ -3,8 +3,12 @@
 const result = document.querySelector(".answer");
 const anyNumberButton = document.querySelectorAll(".number");
 
-for (let i = 0; i < anyNumberButton.length; i++) {
-  anyNumberButton[i].addEventListener("click", function () {
-    result.textContent += anyNumberButton[i].textContent;
-  });
+function init() {
+  for (let i = 0; i < anyNumberButton.length; i++) {
+    anyNumberButton[i].addEventListener("click", function () {
+      result.textContent += anyNumberButton[i].textContent;
+    });
+  }
 }
+
+init();
